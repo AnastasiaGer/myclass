08bcac630b105a2b7b17c64390862fc86cda04a0
AnastasiaGer <aagermanova@gmail.com>
2021-09-01 19:22:18 -0700

    8

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-/* JavaScript код */
+'use strict';
+
+const containers = document.querySelectorAll('.js-buttons-container');
+
+const settingButtonClickHandler = (evt, setting) => {
+  const button = evt.target.closest('button');
+
+  if (!button) {
+    return;
+  }
+
+};
+
+const init = () => {
+  containers.forEach((container) => {
+    const setting = container.dataset;
+
+    container.addEventListener('click', (evt) => {
+      settingButtonClickHandler(evt, setting);
+    });
+  });
+};
+
+init();
