54c21e1b19d14d77f60e2f4f80908f9320067c7d
AnastasiaGer <aagermanova@gmail.com>
2021-09-02 19:53:55 -0700

    done

--- js/script.js
+++ js/script.js
@@ -1,24 +1,53 @@
 'use strict';
 
-const containers = document.querySelectorAll('.js-buttons-container');
+const buttonThemeLight = document.querySelector('.theme-button-light');
+const buttonThemeDark = document.querySelector('.theme-button-dark');
+const buttonThemeTexture = document.querySelector('.theme-button-texture');
 
-const settingButtonClickHandler = (evt, setting) => {
-  const button = evt.target.closest('button');
+const buttonCardViewTile = document.querySelector('.card-view-button-tile');
+const buttonCardViewStandard = document.querySelector('.card-view-button-standard');
+const buttonCardViewCompact = document.querySelector('.card-view-button-compact');
 
-  if (!button) {
-    return;
-  }
+const courses = document.querySelector('.cards');
 
+const buttonThemeLightHandler = () => {
+  document.documentElement.dataset['themeName'] = 'light';
+};
+
+const buttonThemeDarkHandler = () => {
+  document.documentElement.dataset['themeName'] = 'dark';
+};
+
+const buttonThemeTextureHandler = () => {
+  document.documentElement.dataset['themeName'] = 'texture';
+};
+
+const buttonCardViewTileHandler = () => {
+  courses.classList.remove('standard');
+  courses.classList.remove('compact');
+  courses.classList.add('tile');
+};
+
+const buttonCardViewStandardHandler = () => {
+  courses.classList.remove('tile');
+  courses.classList.remove('compact');
+  courses.classList.add('standard');
+};
+
+const buttonCardViewCompactHandler = () => {
+  courses.classList.remove('tile');
+  courses.classList.remove('standard');
+  courses.classList.add('compact');
 };
 
 const init = () => {
-  containers.forEach((container) => {
-    const setting = container.dataset;
+  buttonThemeLight.addEventListener('click', buttonThemeLightHandler);
+  buttonThemeDark.addEventListener('click', buttonThemeDarkHandler);
+  buttonThemeTexture.addEventListener('click', buttonThemeTextureHandler);
 
-    container.addEventListener('click', (evt) => {
-      settingButtonClickHandler(evt, setting);
-    });
-  });
+  buttonCardViewTile.addEventListener('click', buttonCardViewTileHandler);
+  buttonCardViewStandard.addEventListener('click', buttonCardViewStandardHandler);
+  buttonCardViewCompact.addEventListener('click', buttonCardViewCompactHandler);
 };
 
 init();
