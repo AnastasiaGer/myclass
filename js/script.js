67e3be552a89d12f34c744e8110bf6b27ae221f8
AnastasiaGer <aagermanova@gmail.com>
2021-09-02 20:25:38 -0700

    clean code, refactoring

--- js/script.js
+++ js/script.js
@@ -1,53 +1,26 @@
 'use strict';
 
-const buttonThemeLight = document.querySelector('.theme-button-light');
-const buttonThemeDark = document.querySelector('.theme-button-dark');
-const buttonThemeTexture = document.querySelector('.theme-button-texture');
-
-const buttonCardViewTile = document.querySelector('.card-view-button-tile');
-const buttonCardViewStandard = document.querySelector('.card-view-button-standard');
-const buttonCardViewCompact = document.querySelector('.card-view-button-compact');
-
+const btns = Array.from(document.querySelectorAll('.theme-button'));
+const carsViewBts = Array.from(document.querySelectorAll('.card-view-button'));
 const courses = document.querySelector('.cards');
 
-const buttonThemeLightHandler = () => {
-  document.documentElement.dataset['themeName'] = 'light';
+const buttonThemeHandler = (e) => {
+  let theme = e.target.closest('.theme-button').dataset['theme']
+  document.documentElement.dataset['themeName'] = theme;
 };
 
-const buttonThemeDarkHandler = () => {
-  document.documentElement.dataset['themeName'] = 'dark';
-};
-
-const buttonThemeTextureHandler = () => {
-  document.documentElement.dataset['themeName'] = 'texture';
-};
-
-const buttonCardViewTileHandler = () => {
-  courses.classList.remove('standard');
-  courses.classList.remove('compact');
-  courses.classList.add('tile');
-};
-
-const buttonCardViewStandardHandler = () => {
-  courses.classList.remove('tile');
-  courses.classList.remove('compact');
-  courses.classList.add('standard');
-};
-
-const buttonCardViewCompactHandler = () => {
-  courses.classList.remove('tile');
-  courses.classList.remove('standard');
-  courses.classList.add('compact');
+const buttonViewHandler = (e) => {
+  let view = e.target.closest('.card-view-button').dataset['value']
+  carsViewBts.forEach(e => e.classList.remove('active'))
+  e.target.closest('.card-view-button').classList.add('active')
+  courses.className = "";
+  courses.classList.add('cards')
+  courses.classList.add(view)
 };
 
 const init = () => {
-  buttonThemeLight.addEventListener('click', buttonThemeLightHandler);
-  buttonThemeDark.addEventListener('click', buttonThemeDarkHandler);
-  buttonThemeTexture.addEventListener('click', buttonThemeTextureHandler);
-
-  buttonCardViewTile.addEventListener('click', buttonCardViewTileHandler);
-  buttonCardViewStandard.addEventListener('click', buttonCardViewStandardHandler);
-  buttonCardViewCompact.addEventListener('click', buttonCardViewCompactHandler);
+  btns.forEach(btn => btn.addEventListener('click', buttonThemeHandler))
+  carsViewBts.forEach(el => el.addEventListener('click', buttonViewHandler))
 };
 
 init();
